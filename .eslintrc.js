98c1f1ebee036dffb214c7d36473382fca48e6a6
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-12 21:10:13 +0400

    style(.eslintrc.js)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
     ecmaFeatures: {
