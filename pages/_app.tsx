f663eb1f2546090bdf9a6d42be1d18de90b32434
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-12 21:02:25 +0400

    refactor(src/components/footer)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Head from "next/head";
-import { Footer } from "../src/components/footer.component";
 import "../src/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Footer } from "../src/components/footer";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
