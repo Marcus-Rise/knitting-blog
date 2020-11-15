54f2acea6f9a2961d856d155820099729dd7aebf
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-15 11:49:59 +0400

    feat(post.service.ts)

---
+++ src/post/post.service.ts
@@ -0,0 +1,23 @@
+import type { IPostService } from "./post.service.interface";
+import type { IPost } from "./post.interface";
+import { inject, injectable } from "inversify";
+import type { IPostRepository } from "./post.repository.interface";
+import { POST_REPOSITORY_PROVIDER } from "./post.repository.interface";
+
+@injectable()
+class PostService implements IPostService {
+  constructor(
+    @inject(POST_REPOSITORY_PROVIDER)
+    private readonly repo: IPostRepository,
+  ) {}
+
+  async getBySlug(slug: string): Promise<IPost | null> {
+    return this.repo.find({ slug });
+  }
+
+  async getList(limit: number, offset: number): Promise<IPost[]> {
+    return this.repo.list({}, limit, offset);
+  }
+}
+
+export { PostService };
