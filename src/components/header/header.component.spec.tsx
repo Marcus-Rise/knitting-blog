b1067af80902fc4ea51127351af49a1f31567492
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-22 19:51:17 +0400

    test(header.component.spec.tsx)

--- src/components/header/header.component.spec.tsx
+++ src/components/header/header.component.spec.tsx
@@ -2,6 +2,17 @@ import { render } from "@testing-library/react";
 import { Header } from "./header.component";
 import React from "react";
 
+jest.mock("../nav", () => ({
+  __esModule: true,
+  Nav: jest.fn(() => <nav />),
+}));
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: jest.fn((props) => (
+    <img src={props.logoSrc} alt={props.title} height={props.logoSize} width={props.logoSize} />
+  )),
+}));
+
 describe("Header", () => {
   test("render", () => {
     const { asFragment } = render(
