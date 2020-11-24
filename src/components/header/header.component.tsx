77a725ed25f369f88c1533888b2a24ea3ff9a309
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-24 09:36:03 +0400

    feat(header): link to main

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "./header.module.scss";
 import type { INavLink } from "../nav";
 import { Nav } from "../nav";
+import Link from "next/link";
 
 interface IProps {
   title: string;
@@ -13,12 +14,16 @@ interface IProps {
 const Header: React.FC<IProps> = (props) => {
   return (
     <header className={styles.root}>
-      <div className={styles.logoContainer}>
-        <div className={styles.logo}>
-          <Image src={props.logoSrc} alt={props.title} height={"auto"} width={"auto"} layout={"responsive"} />
-        </div>
-        <h1 className={styles.title}>{props.title}</h1>
-      </div>
+      <Link href={"/"}>
+        <a className={styles.link}>
+          <div className={styles.logoContainer}>
+            <div className={styles.logo}>
+              <Image src={props.logoSrc} alt={props.title} height={"auto"} width={"auto"} layout={"responsive"} />
+            </div>
+            <h1 className={styles.title}>{props.title}</h1>
+          </div>
+        </a>
+      </Link>
       <Nav items={props.links} />
     </header>
   );
