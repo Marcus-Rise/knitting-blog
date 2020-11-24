9aad005d61daa56b703c59f69c5a676df1bcd88d
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-24 11:44:37 +0400

    feat(seo.module.ts)

--- src/seo/seo.module.ts
+++ src/seo/seo.module.ts
@@ -1,10 +1,10 @@
 import { ContainerModule } from "inversify";
 import type { ISeoService } from "./seo.service.interface";
 import { SEO_SERVICE_PROVIDER } from "./seo.service.interface";
-import { SeoServiceMock } from "./seo.service.mock";
+import { SeoService } from "./seo.service";
 
 const SeoModule = new ContainerModule((bind) => {
-  bind<ISeoService>(SEO_SERVICE_PROVIDER).to(SeoServiceMock).inSingletonScope();
+  bind<ISeoService>(SEO_SERVICE_PROVIDER).to(SeoService).inSingletonScope();
 });
 
 export { SeoModule };
