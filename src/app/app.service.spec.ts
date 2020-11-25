fbdb89622ab7c73bfb33a2605c251ece86e6fc46
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-25 10:00:55 +0400

    test(app.service.spec.ts): split tests

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -2,10 +2,14 @@ import "reflect-metadata";
 import { AppService } from "./app.service";
 
 describe("AppService", () => {
-  test("siteConfig", () => {
-    const { author, title } = new AppService();
+  test("title", () => {
+    const { title } = new AppService();
 
     expect(title).toEqual("Надя вяжет");
+  });
+  test("author", () => {
+    const { author } = new AppService();
+
     expect(author.name).toEqual("Ilya Konstantinov");
     expect(author.url).toEqual("https://marcus-rise.dev");
   });
