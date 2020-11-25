dde3dda9c4a749028b33a64e4ad290e554d4b6e8
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-25 15:13:16 +0400

    feat(pages/_app.tsx): site verification codes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,13 @@ import type { INavLink } from "../src/components/nav";
 import { useInject } from "../src/ioc";
 import type { IAppService } from "../src/app";
 import { APP_SERVICE_PROVIDER } from "../src/app";
+import type { ISeoConfigService } from "../src/seo";
+import { SEO_CONFIG_SERVICE_PROVIDER } from "../src/seo";
+import { VerificationCodes } from "../src/seo/verification-codes";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const { title, author } = useInject<IAppService>(APP_SERVICE_PROVIDER);
+  const { googleVerificationCode, yandexVerificationCode } = useInject<ISeoConfigService>(SEO_CONFIG_SERVICE_PROVIDER);
 
   const links: Array<INavLink> = [
     {
@@ -42,6 +46,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
         <title key={"title"}>{title}</title>
         <meta key={"meta-title"} name={"title"} content={title} />
       </Head>
+      <VerificationCodes googleCode={googleVerificationCode} yandexCode={yandexVerificationCode} />
       <Header title={title} logoSrc={"/logo.svg"} links={links} />
       <main>
         <Component {...pageProps} />
