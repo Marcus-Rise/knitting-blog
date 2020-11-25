4692ad61161cc58c049612e217cb3229a3fb7ad6
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-25 11:41:35 +0400

    fix(post-prismic.dto.ts): empty data

--- src/post/post-prismic.dto.ts
+++ src/post/post-prismic.dto.ts
@@ -1,5 +1,5 @@
 import type { IPost } from "./post.interface";
-import type { IPostPrismicDto } from "./post-prismic.dto.interface";
+import type { Image, IPostPrismicDto } from "./post-prismic.dto.interface";
 import { SliceTypeEnum } from "./post-prismic.dto.interface";
 import type { IImageGallery, IPostContent } from "./post-content.inteface";
 import type { RichTextBlock } from "prismic-reactjs";
@@ -16,29 +16,50 @@ class PostPrismicDto implements IPost {
   constructor(dto: IPostPrismicDto) {
     this.title = dto.data.title[0].text;
     this.slug = dto.uid ?? dto.slugs[0];
-    this.description = dto.data.description[0].text;
-    this.imageSrc = dto.data.main_image.url;
-    this.imageLabel = dto.data.main_image.alt ?? "";
+
+    if (dto.data.description.length) {
+      this.description = dto.data.description[0].text;
+    } else {
+      this.description = "";
+    }
+
+    if (PostPrismicDto.isMainImage(dto.data.main_image)) {
+      this.imageSrc = dto.data.main_image.url;
+      this.imageLabel = dto.data.main_image.alt ?? "";
+    } else {
+      this.imageSrc = "";
+      this.imageLabel = "";
+    }
+
     this.date = dto.first_publication_date ?? "";
-    this.content = dto.data.body.flatMap<RichTextBlock | IImageGallery>((i) => {
-      switch (i.slice_type) {
-        case SliceTypeEnum.TEXT:
-          return i.primary.text.flat();
-        case SliceTypeEnum.IMAGE_GALLERY:
-          return <IImageGallery>{
-            type: i.slice_type,
-            label: i.primary.name_of_the_gallery[0] ?? null,
-            items: i.items.map((image) => ({
-              alt: image.gallery_image.alt,
-              url: image.gallery_image.url,
-              height: image.gallery_image.dimensions.height,
-              width: image.gallery_image.dimensions.width,
-            })),
-          };
-        default:
-          return [];
-      }
-    });
+
+    if (dto.data.body.length) {
+      this.content = dto.data.body.flatMap<RichTextBlock | IImageGallery>((i) => {
+        switch (i.slice_type) {
+          case SliceTypeEnum.TEXT:
+            return i.primary.text.flat();
+          case SliceTypeEnum.IMAGE_GALLERY:
+            return <IImageGallery>{
+              type: i.slice_type,
+              label: i.primary.name_of_the_gallery[0] ?? null,
+              items: i.items.map((image) => ({
+                alt: image.gallery_image.alt,
+                url: image.gallery_image.url,
+                height: image.gallery_image.dimensions.height,
+                width: image.gallery_image.dimensions.width,
+              })),
+            };
+          default:
+            return [];
+        }
+      });
+    } else {
+      this.content = [];
+    }
+  }
+
+  private static isMainImage(obj: Record<string, unknown> | Image): obj is Image {
+    return "url" in obj;
   }
 }
 
