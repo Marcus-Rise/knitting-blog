3d157d217d5eea978364d78647e3437ff8404fc5
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-26 10:25:14 +0400

    test(image-view.component.spec.tsx)

---
+++ src/components/image-view/image-view.component.spec.tsx
@@ -0,0 +1,44 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import { ImageView } from "./image-view.component";
+import React from "react";
+
+jest.mock("../modal", () => ({
+  __esModule: true,
+  Modal: jest.fn((props) => <div data-testid={"modal"}>{props.children}</div>),
+}));
+
+describe("ImageView", () => {
+  const items = [
+    { src: "/src1", alt: "alt" },
+    { src: "/src2", alt: "alt" },
+    { src: "/src3", alt: "alt" },
+  ];
+  test("close", () => {
+    const [item] = items;
+    render(
+      <ImageView alt={item.alt} src={item.src}>
+        <div data-testid="children" />
+      </ImageView>,
+    );
+
+    expect(screen.queryByTestId("modal")).toBeNull();
+
+    const children = screen.queryByTestId("children");
+    expect(children).not.toBeNull();
+
+    if (children) {
+      fireEvent.click(children);
+    }
+
+    expect(screen.queryByTestId("modal")).not.toBeNull();
+
+    const closeButton = screen.queryByText("X");
+    expect(closeButton).not.toBeNull();
+
+    if (closeButton) {
+      fireEvent.click(closeButton);
+    }
+
+    expect(screen.queryByTestId("modal")).toBeNull();
+  });
+});
