196edc263a90a3e25f4a9f140b6d795c6807feba
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-29 23:20:54 +0400

    test(pages/api/exit-preview.spec.ts)

---
+++ pages/api/exit-preview.spec.ts
@@ -0,0 +1,22 @@
+import handler from "./exit-preview";
+import { mock } from "jest-mock-extended";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+describe("exit-preview", () => {
+  test("host", () => {
+    let res: unknown[] = [];
+
+    handler(
+      mock<NextApiRequest>(),
+      mock<NextApiResponse>({
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        writeHead: (...args) => (res = args),
+      }),
+    );
+
+    const [code, body] = res;
+    expect(code).toBe(307);
+    expect(body).toEqual({ Location: "/" });
+  });
+});
