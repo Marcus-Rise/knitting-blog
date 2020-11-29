639a288aeed51ec28103c2b6df647be6eb0b1fca
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-29 21:39:58 +0400

    test(pages/api/robots.spec.ts)

---
+++ pages/api/robots.spec.ts
@@ -0,0 +1,24 @@
+import handler from "./robots";
+import { mock } from "jest-mock-extended";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { ISeoService } from "../../src/seo";
+
+describe("robots", () => {
+  test("host", async () => {
+    let res = "";
+
+    await handler(
+      mock<NextApiRequest>({
+        headers: { host: "host.com" },
+      }),
+      mock<NextApiResponse>({
+        end: (response: unknown) => (res = String(response)),
+      }),
+      mock<ISeoService>({
+        generateRobotsTxt: (hostName) => Promise.resolve(hostName),
+      }),
+    );
+
+    expect(res).toEqual("host.com");
+  });
+});
