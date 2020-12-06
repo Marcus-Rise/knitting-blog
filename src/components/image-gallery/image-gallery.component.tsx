5fb54542f35b233a735f6f3759aeb1d394bd0a90
Ilya Konstantinov <ilykonst95@gmail.com>
2020-12-06 18:12:02 +0400

    feat(image-gallery): use view box

--- src/components/image-gallery/image-gallery.component.tsx
+++ src/components/image-gallery/image-gallery.component.tsx
@@ -2,13 +2,27 @@ import React, { useMemo } from "react";
 import type { IImage } from "../image";
 import { Image } from "../image";
 import styles from "./image-gallery.module.scss";
+import { ImageView } from "../image-view";
 
 const ImageGallery: React.FC<{
   items: IImage[];
 }> = (props) => {
-  const items = useMemo(() => props.items.map((i, index) => <Image key={i.src + index} {...i} />), [props.items]);
+  const album = useMemo(() => props.items.map((i) => ({ src: i.src, alt: i.alt ?? "" })), [props.items]);
+  const items = useMemo(
+    () =>
+      props.items.map((i, index) => (
+        <ImageView key={i.src + index} currentIndex={index} album={album}>
+          <Image {...i} />
+        </ImageView>
+      )),
+    [props.items, album],
+  );
 
-  return <div className={styles.root}>{items}</div>;
+  return (
+    <>
+      <div className={styles.root}>{items}</div>
+    </>
+  );
 };
 
 export { ImageGallery };
