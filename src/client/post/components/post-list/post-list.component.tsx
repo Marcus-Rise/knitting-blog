00ceaae774a3a4caaab374187bb2c317eb28cf1d
Marcus-Rise <ilykonst95@gmail.com>
2021-08-21 19:12:05 +0400

    post list

--- src/client/post/components/post-list/post-list.component.tsx
+++ src/client/post/components/post-list/post-list.component.tsx
@@ -1,18 +1,22 @@
 import type { FC } from "react";
 import React from "react";
 import { PostListItem } from "../post-list-item";
-import styles from "./post-list.module.scss";
 import type { IPost } from "../../../../common/post";
+import styled from "styled-components";
 
 interface IProps {
   items: Array<IPost>;
 }
 
+const Item = styled.div`
+  margin-bottom: 1rem;
+`;
+
 const PostList: FC<IProps> = ({ items }) => {
   const posts = items.map((i, index) => {
     return (
       <div className="col-12" key={index}>
-        <PostListItem className={styles.item} {...i} />
+        <Item as={PostListItem} {...i} />
       </div>
     );
   });
