b7cb3b205d31d2daa81f022099ea643085b15cc3
Marcus-Rise <ilykonst95@gmail.com>
2021-08-21 17:06:05 +0400

    footer

--- src/client/components/footer/footer.component.tsx
+++ src/client/components/footer/footer.component.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 import React from "react";
-import styles from "./footer.module.scss";
+import styled from "styled-components";
 
 interface IProps {
   year: string | number;
@@ -8,13 +8,23 @@ interface IProps {
   authorLink: string;
 }
 
+const Root = styled.footer`
+  background-color: #dbdbdb;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  padding: 1rem;
+`;
+
 const Footer: FC<IProps> = ({ author, authorLink, year }) => (
-  <footer className={styles.footer}>
+  <Root>
     <span className="mr-1">&#9400; {year}</span>
     <a href={authorLink} target={"_blank"} rel="noreferrer">
       {author}
     </a>
-  </footer>
+  </Root>
 );
 
 export { Footer };
