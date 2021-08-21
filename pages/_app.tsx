8b9be5da6050527a3802b6ec081b82948e1d478e
Marcus-Rise <ilykonst95@gmail.com>
2021-08-21 19:36:01 +0400

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,10 @@ import "reflect-metadata";
 import type { FC } from "react";
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "../src/client/styles";
-import Head from "next/head";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <GlobalStyles />
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"crossorigin"} />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
     <Component {...pageProps} />
   </>
 );
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -20,6 +20,16 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"crossorigin"} />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap"
+            rel="stylesheet"
+          />
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
