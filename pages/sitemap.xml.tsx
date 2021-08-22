93423faece97fc690ec46ea8dbb2675273eddab6
Marcus-Rise <ilykonst95@gmail.com>
2021-08-22 22:24:15 +0400

    fix sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,12 +1,13 @@
 import type { GetServerSideProps } from "next";
 import type { ISeoService } from "../src/server";
 import { inject, SEO_SERVICE_PROVIDER } from "../src/server";
+import * as punycode from "punycode";
 
 const getServerSideProps: GetServerSideProps = async (
   { req, res },
   seo = inject<ISeoService>(SEO_SERVICE_PROVIDER),
 ) => {
-  const hostName = String(req.headers.host);
+  const hostName = punycode.toUnicode(String(req.headers.host));
   const sitemap = await seo.generateSitemap(hostName);
 
   res.setHeader("Content-Type", "application/xml");
