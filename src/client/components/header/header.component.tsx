f06bc996d28bacaed55089ba17f5f21a40e50d9b
Marcus-Rise <ilykonst95@gmail.com>
2021-08-22 19:04:26 +0400

    logo mobile

--- src/client/components/header/header.component.tsx
+++ src/client/components/header/header.component.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import type { INavLink } from "../nav";
 import { Nav } from "../nav";
 import Link from "next/link";
-import { Container, LinkStyled, Root, Title } from "./header.styles";
+import NextImage from "next/image";
+import { Container, Image, LinkStyled, Root, Title } from "./header.styles";
+import Logo from "../../../../public/android-chrome-192x192.png";
 
 interface IProps {
   title: string;
   links: ReadonlyArray<INavLink>;
 }
+
+const LOGO_SIZE = 35;
 
 const Header: FC<IProps> = ({ links, title }) => (
   <Root>
@@ -16,6 +20,14 @@ const Header: FC<IProps> = ({ links, title }) => (
       <LinkStyled>
         <Container>
           <Title>{title}</Title>
+          <Image
+            as={NextImage}
+            src={Logo}
+            alt={title}
+            height={LOGO_SIZE}
+            width={LOGO_SIZE}
+            quality={15}
+          />
         </Container>
       </LinkStyled>
     </Link>
