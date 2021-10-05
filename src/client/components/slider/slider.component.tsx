680cdea1d2b06f05fc613736fd2dea4c87044224
Marcus-Rise <ilykonst95@gmail.com>
2021-10-05 14:30:34 +0400

    fix(slider)

--- src/client/components/slider/slider.component.tsx
+++ src/client/components/slider/slider.component.tsx
@@ -171,6 +171,7 @@ const Slider: FC<ISliderProps> = ({ images, onClose, startIndex = 0 }) => {
 
   const keyDownEventHandler = useCallback(
     (e: KeyboardEvent) => {
+      e.preventDefault();
       switch (e.code) {
         case "ArrowLeft": {
           navigateBack();
