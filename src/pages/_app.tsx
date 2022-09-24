81a2adbfc7ed23fa1fcd2c65793e927968f76a53
Marcus-Rise <ilykonst95@gmail.com>
2022-09-24 19:02:06 +0400

    types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import type { FC } from "react";
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default MyApp;
