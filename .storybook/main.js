f2e03dd16f75de32a85d3b9497a8caacfce718cd
Marcus-Rise <ilykonst95@gmail.com>
2022-09-24 19:01:32 +0400

    global styles

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,14 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "storybook-addon-sass-postcss",
+      options: {
+        sassLoaderOptions: {
+          implementation: require("sass"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
