302639568741f891995f5d0f0340a522bcb9618e
Marcus-Rise <ilykonst95@gmail.com>
2022-09-26 12:17:29 +0400

    fix: stories

--- src/post/components/card/post-card.stories.tsx
+++ src/post/components/card/post-card.stories.tsx
@@ -14,8 +14,12 @@ Default.args = {
   description:
     "Да, флоксы хороши именно вечером. Вся эта душная и густая полифония полудня стихает, и ты улавливаешь тонкую струйку аромата из самой сердцевины летней роскоши. Многообразие оттенков вдохновляет, но мой фаворит вот этот, закатно-фиолетовый, чуть подкрашенный сумерками. Кстати, у «Charme» от Vita много чудесных оттенков.",
   slug: "post-slug",
-  imageSrc:
-    "https://images.prismic.io/nextjs-starter-prismic-blog/fb66c550-06ee-406b-8be3-06ac639f7bb0_laura-kennedy-VVUOodsIwj4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1404%2C3744%2C2808&w=3840&fit=max",
+  image: {
+    src: "https://images.prismic.io/nextjs-starter-prismic-blog/fb66c550-06ee-406b-8be3-06ac639f7bb0_laura-kennedy-VVUOodsIwj4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1404%2C3744%2C2808&w=3840&fit=max",
+    alt: "Сидит заяц на заборе",
+    height: 1280,
+    width: 852,
+  },
   date: "2020-12-21T17:56:12+0000",
 };
 
