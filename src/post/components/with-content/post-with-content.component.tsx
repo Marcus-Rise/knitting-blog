d120f03b37720c81dd210ebd2e789b5e7bc66ba7
Marcus-Rise <ilykonst95@gmail.com>
2022-09-27 17:01:32 +0400

    feat: post content

--- src/post/components/with-content/post-with-content.component.tsx
+++ src/post/components/with-content/post-with-content.component.tsx
@@ -4,6 +4,7 @@ import { Title } from "../../../components/title";
 import styles from "./post-with-content.module.scss";
 import { Hr } from "../../../components/hr";
 import { DateComponent } from "../../../components/date";
+import NextImage from "next/future/image";
 
 const PostWithContent: FC<Omit<PostWithContentModel, "description">> = ({
   title,
@@ -16,6 +17,14 @@ const PostWithContent: FC<Omit<PostWithContentModel, "description">> = ({
       <Title className={styles.title}>{title}</Title>
       <Hr />
       <DateComponent className={styles.date} date={date} />
+      <NextImage
+        src={image.src}
+        alt={image.alt}
+        height={image.height}
+        width={image.width}
+        className={styles.image}
+        priority
+      />
     </>
   );
 };
