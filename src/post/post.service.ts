10d1a7eef0889b763976768e2d36f5ad4e222639
Marcus-Rise <ilykonst95@gmail.com>
2022-09-27 13:24:44 +0400

    fix: posts order

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -5,7 +5,12 @@ import { client } from "../prismic/prismic-client";
 
 class PostService {
   static async getList(): Promise<PostPreviewModel[]> {
-    const dto: Array<PostDocument> = await client.getAllByType("post");
+    const dto: Array<PostDocument> = await client.getAllByType("post", {
+      orderings: {
+        field: "document.first_publication_date",
+        direction: "desc",
+      },
+    });
 
     return dto.map((i) => PostPreviewModelFactory.fromResponseDto(i));
   }
