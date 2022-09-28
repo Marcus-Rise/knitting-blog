2859e544b7b11cf863c7bbfdd906fbdeff424631
Marcus-Rise <ilykonst95@gmail.com>
2022-09-28 14:35:43 +0400

    Revert "feat: remove prismic toolbar"
    This reverts commit 3c4e5b933e7b6d1cfbed179bda2cd61cc105f22d.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,26 @@ import type { FC } from "react";
 import { Layout } from "../components/layout";
 import "../styles/global.scss";
 import { config } from "../config";
+import { PrismicPreview } from "@prismicio/next";
+import { PrismicProvider } from "@prismicio/react";
+import Link from "next/link";
+import { linkResolver } from "../prismic/prismic-link-resolver";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Layout title={config.title} authorName={config.author.name} authorLink={config.author.url}>
-    <Component {...pageProps} />
-  </Layout>
+  <PrismicProvider
+    linkResolver={linkResolver}
+    internalLinkComponent={({ href, ...props }) => (
+      <Link href={href}>
+        <a {...props} />
+      </Link>
+    )}
+  >
+    <PrismicPreview repositoryName={process.env.NEXT_PUBLIC_PRISMIC_REPOSITORY ?? ""}>
+      <Layout title={config.title} authorName={config.author.name} authorLink={config.author.url}>
+        <Component {...pageProps} />
+      </Layout>
+    </PrismicPreview>
+  </PrismicProvider>
 );
 
 export default MyApp;
