3c4e5b933e7b6d1cfbed179bda2cd61cc105f22d
Marcus-Rise <ilykonst95@gmail.com>
2022-09-28 14:33:36 +0400

    feat: remove prismic toolbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,26 +3,11 @@ import type { FC } from "react";
 import { Layout } from "../components/layout";
 import "../styles/global.scss";
 import { config } from "../config";
-import { PrismicPreview } from "@prismicio/next";
-import { PrismicProvider } from "@prismicio/react";
-import Link from "next/link";
-import { linkResolver } from "../prismic/prismic-link-resolver";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <PrismicProvider
-    linkResolver={linkResolver}
-    internalLinkComponent={({ href, ...props }) => (
-      <Link href={href}>
-        <a {...props} />
-      </Link>
-    )}
-  >
-    <PrismicPreview repositoryName={process.env.NEXT_PUBLIC_PRISMIC_REPOSITORY ?? ""}>
-      <Layout title={config.title} authorName={config.author.name} authorLink={config.author.url}>
-        <Component {...pageProps} />
-      </Layout>
-    </PrismicPreview>
-  </PrismicProvider>
+  <Layout title={config.title} authorName={config.author.name} authorLink={config.author.url}>
+    <Component {...pageProps} />
+  </Layout>
 );
 
 export default MyApp;
