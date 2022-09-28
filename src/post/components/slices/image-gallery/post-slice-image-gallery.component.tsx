2efd61c650bb21e6b4dacedfcce7768ce6250585
Marcus-Rise <ilykonst95@gmail.com>
2022-09-28 16:20:27 +0400

    feat: slider

--- src/post/components/slices/image-gallery/post-slice-image-gallery.component.tsx
+++ src/post/components/slices/image-gallery/post-slice-image-gallery.component.tsx
@@ -1,21 +1,48 @@
 import type { FC } from "react";
+import { useCallback, useMemo, useState } from "react";
 import type { PostDocumentDataBodyImageGallerySlice } from "../../../../prismic";
 import NextImage from "next/future/image";
 import styles from "./post-slice-image-gallery.module.scss";
+import dynamic from "next/dynamic";
 
-const PostSliceImageGallery: FC<{ slice: PostDocumentDataBodyImageGallerySlice }> = ({ slice }) => (
-  <div className={styles.wrapper}>
-    {slice.items.map(({ gallery_image: image }, index) => (
-      <NextImage
-        key={index}
-        src={image.url ?? ""}
-        alt={image.alt ?? ""}
-        height={300}
-        width={300}
-        className={styles.image}
-      />
-    ))}
-  </div>
+const Slider = dynamic(() =>
+  import("../../../../components/slider").then((module) => module.Slider),
 );
+
+const PostSliceImageGallery: FC<{ slice: PostDocumentDataBodyImageGallerySlice }> = ({ slice }) => {
+  const [sliderStartIndex, setSliderStartIndex] = useState<number | null>(null);
+
+  const images = useMemo(
+    () =>
+      slice.items.map((i) => {
+        return {
+          url: i.gallery_image.url ?? "",
+          alt: i.gallery_image.alt ?? "",
+        };
+      }),
+    [slice.items],
+  );
+
+  const closeSlider = useCallback(() => setSliderStartIndex(null), []);
+
+  return (
+    <div className={styles.wrapper}>
+      {images.map((image, index) => (
+        <NextImage
+          key={index}
+          src={image.url}
+          alt={image.alt}
+          height={300}
+          width={300}
+          className={styles.image}
+          onClick={() => setSliderStartIndex(index)}
+        />
+      ))}
+      {sliderStartIndex !== null && (
+        <Slider startIndex={sliderStartIndex} images={images} onClose={closeSlider} />
+      )}
+    </div>
+  );
+};
 
 export { PostSliceImageGallery };
