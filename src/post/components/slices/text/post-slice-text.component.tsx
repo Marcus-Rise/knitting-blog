44e824acce8571bc956adbfd09dce366b756efed
Ilya Konstantinov <ilykonst95@gmail.com>
2022-09-29 12:31:40 +0400

    fix: typography (#110)

--- src/post/components/slices/text/post-slice-text.component.tsx
+++ src/post/components/slices/text/post-slice-text.component.tsx
@@ -2,13 +2,8 @@ import type { FC } from "react";
 import type { PostDocumentDataBodyTextSlice } from "../../../../prismic";
 import { isFilled } from "@prismicio/helpers";
 import { PrismicRichText } from "@prismicio/react";
-import styles from "./post-slice-text.module.scss";
 
 const PostSliceText: FC<{ slice: PostDocumentDataBodyTextSlice }> = ({ slice }) =>
-  isFilled.richText(slice.primary.text) ? (
-    <div className={styles.wrapper}>
-      <PrismicRichText field={slice.primary.text} />
-    </div>
-  ) : null;
+  isFilled.richText(slice.primary.text) ? <PrismicRichText field={slice.primary.text} /> : null;
 
 export { PostSliceText };
