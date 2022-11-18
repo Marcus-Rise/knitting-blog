9cca02bf5dbc1a326f547d378dd36c7ea54469e8
Marcus-Rise <ilykonst95@gmail.com>
2022-11-18 19:32:22 +0400

    refactor

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -7,16 +7,14 @@ const Footer: FC<{ authorName: string; authorLink: string; year: string }> = ({
   authorName,
   authorLink,
   year,
-}) => {
-  return (
-    <footer className={styles.footer}>
-      <Theme className={styles.theme} />
-      <span>&#9400; {year}</span>
-      <Link className={styles.link} href={authorLink} target={"_blank"} rel="noreferrer">
-        {authorName}
-      </Link>
-    </footer>
-  );
-};
+}) => (
+  <footer className={styles.footer}>
+    <Theme className={styles.theme} />
+    <span>&#9400; {year}</span>
+    <Link className={styles.link} href={authorLink} target={"_blank"} rel="noreferrer">
+      {authorName}
+    </Link>
+  </footer>
+);
 
 export { Footer };
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -7,24 +7,22 @@ const LOGO_SIZE = 35;
 
 type Props = { title: string };
 
-const Header: FC<Props> = ({ title }) => {
-  return (
-    <header className={styles.header}>
-      <Link className={styles.link} href={"/"}>
-        <div className={styles.logo}>
-          <h1 className={styles.title}>{title}</h1>
-          <Image
-            alt={title}
-            src={"/android-chrome-192x192.png"}
-            height={LOGO_SIZE}
-            width={LOGO_SIZE}
-            quality={15}
-            className={styles.image}
-          />
-        </div>
-      </Link>
-    </header>
-  );
-};
+const Header: FC<Props> = ({ title }) => (
+  <header className={styles.header}>
+    <Link className={styles.link} href={"/"}>
+      <div className={styles.logo}>
+        <h1 className={styles.title}>{title}</h1>
+        <Image
+          alt={title}
+          src={"/android-chrome-192x192.png"}
+          height={LOGO_SIZE}
+          width={LOGO_SIZE}
+          quality={15}
+          className={styles.image}
+        />
+      </div>
+    </Link>
+  </header>
+);
 
 export { Header };
