775554983870e4d5f78dab339a8a4298a17904c7
Marcus-Rise <ilykonst95@gmail.com>
2022-11-25 14:03:28 +0400

    build

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withPWA = require("next-pwa")({
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true, enableUndici: notProduction },
+  experimental: { appDir: true },
   reactStrictMode: true,
   images: {
     domains: ["images.prismic.io"],
