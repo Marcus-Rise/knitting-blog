f9ca24450e23ace547e94343aec58ab8a762c525
Marcus-Rise <ilykonst95@gmail.com>
2022-11-28 13:24:51 +0400

    chore: remove seo module

--- src/server/ioc/container.ts
+++ src/server/ioc/container.ts
@@ -2,9 +2,8 @@ import "reflect-metadata";
 import { Container } from "inversify";
 import { PostModule } from "../post/post.module";
 import { HttpModule } from "../utils/http/http.module";
-import { SeoModule } from "../seo/seo.module";
 
 const container = new Container({ defaultScope: "Singleton" });
-container.load(HttpModule, PostModule, SeoModule);
+container.load(HttpModule, PostModule);
 
 export { container };
