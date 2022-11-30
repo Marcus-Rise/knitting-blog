9ab18cbf314b336ec9f3f9caac1d3de32ba79bc6
Marcus-Rise <ilykonst95@gmail.com>
2022-11-30 11:35:39 +0400

    fix: skip waiting

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,3 +7,12 @@ self.addEventListener("push", (event) => {
     }),
   );
 });
+
+self.addEventListener("install", (event) => {
+  // The promise that skipWaiting() returns can be safely ignored.
+  self.skipWaiting();
+
+  // Perform any other actions required for your
+  // service worker to install, potentially inside
+  // of event.waitUntil();
+});
