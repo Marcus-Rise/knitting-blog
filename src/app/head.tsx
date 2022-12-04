3a513385485dda8bab8f5760cd95293414ca969c
Marcus-Rise <ilykonst95@gmail.com>
2022-12-04 19:57:32 +0400

    fix: viewport

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -10,7 +10,7 @@ const Head = async () => {
       <meta charSet="UTF-8" />
       <meta
         name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
       />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       <meta name="color-scheme" content="dark light" />
