83d8f55a5a65d356482d108573e7d632c8ffcc69
Marcus-Rise <ilykonst95@gmail.com>
2022-12-04 20:04:35 +0400

    fix: head

--- src/app/[slug]/head.tsx
+++ src/app/[slug]/head.tsx
@@ -16,6 +16,20 @@ const Head = async ({ params }: { params: Params }) => {
 
   return (
     <>
+      <meta charSet="UTF-8" />
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+      />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="color-scheme" content="dark light" />
+      <meta name="theme-color" content={"#fff"} />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+
       <title key={"title"}>{title}</title>
       <meta key={"meta-title"} name={"title"} content={title} />
       <meta key={"description"} name={"description"} content={post.description} />
