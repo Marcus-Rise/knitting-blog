f1701af622d63dcb30ffcc0c93c0b9fd88c71c9e
Marcus-Rise <ilykonst95@gmail.com>
2022-12-05 16:44:59 +0400

    Revert "perf: suspense"
    This reverts commit 4fe6dc370fd3d4ddf2146ce35f2c2e793e203b93.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,33 @@
 import { PostCard } from "../post/components/card";
 import { Container } from "../components/container";
 import { getPosts } from "../server";
-import React, { Suspense } from "react";
 
 const Home = async () => {
   const [firstPost, ...posts] = await getPosts();
 
   const cards = posts.map((post) => (
-    <Suspense key={post.slug}>
-      <PostCard
-        title={post.title}
-        description={post.description}
-        image={post.image}
-        slug={post.slug}
-        date={post.date}
-      />
-    </Suspense>
+    <PostCard
+      key={post.slug}
+      title={post.title}
+      description={post.description}
+      image={post.image}
+      slug={post.slug}
+      date={post.date}
+    />
   ));
 
   return (
     <Container>
       <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
-        <Suspense>
-          <PostCard
-            key={firstPost.slug}
-            slug={firstPost.slug}
-            priorityImage
-            image={firstPost.image}
-            title={firstPost.title}
-            date={firstPost.date}
-            description={firstPost.description}
-          />
-        </Suspense>
+        <PostCard
+          key={firstPost.slug}
+          slug={firstPost.slug}
+          priorityImage
+          image={firstPost.image}
+          title={firstPost.title}
+          date={firstPost.date}
+          description={firstPost.description}
+        />
         {cards}
       </div>
     </Container>
