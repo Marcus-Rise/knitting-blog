cd44de88aea59442798ecf92217f700d671867c9
Ilya Konstantinov <ilykonst95@gmail.com>
2023-05-30 15:58:25 +0400

    Refactor (#115)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,20 +1,27 @@
 import type { MetadataRoute } from "next";
 import { headers } from "next/headers";
 
+type Rules = MetadataRoute.Robots["rules"];
+
+const allowRules: Rules = {
+  userAgent: "*",
+  allow: "/",
+};
+const disallowRules: Rules = {
+  userAgent: "*",
+  disallow: "/",
+};
+
 const robots = (): MetadataRoute.Robots => {
   const isAllow = Boolean(process.env.ALLOW_ROBOTS);
   const host = headers().get("Host") ?? "";
+  const baseUrl = new URL(`https://${host}`);
+
+  const sitemap = new URL(`/sitemap.xml`, baseUrl).href;
 
   return {
-    rules: isAllow
-      ? {
-          userAgent: "*",
-          allow: "/",
-        }
-      : {
-          disallow: "/",
-        },
-    sitemap: `https://${host}/sitemap.xml`,
+    rules: isAllow ? allowRules : disallowRules,
+    sitemap,
   };
 };
 
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,24 +2,24 @@ import type { MetadataRoute } from "next";
 import { headers } from "next/headers";
 import { getPosts } from "../server";
 
+type Page = MetadataRoute.Sitemap[number];
+
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
   const host = headers().get("Host") ?? "";
+  const baseUrl = new URL(`https://${host}`);
+
   const [firstPost, ...posts] = await getPosts();
 
-  return [
-    {
-      url: new URL(`https://${host}/`).href,
-      lastModified: firstPost.date,
-    },
-    {
-      url: new URL(`https://${host}/${firstPost.slug}`).href,
-      lastModified: firstPost.date,
-    },
-    ...posts.map((post) => ({
-      url: new URL(`https://${host}/${post.slug}`).href,
-      lastModified: post.date,
-    })),
-  ];
+  const mainPage: Page = {
+    url: new URL("/", baseUrl).href,
+    lastModified: firstPost.date,
+  };
+  const postPages = [firstPost, ...posts].map((post) => ({
+    url: new URL(`/${post.slug}`, baseUrl).href,
+    lastModified: post.date,
+  }));
+
+  return [mainPage, ...postPages];
 };
 
 export default sitemap;
