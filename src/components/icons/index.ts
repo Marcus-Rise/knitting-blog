e2105f8d605999b82812b08a9a3a2e38307707c1
Marcus-Rise <ilykonst95@gmail.com>
2023-05-31 12:16:19 +0400

    feat: telegram link

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,4 @@ export * from "./moon-icon.component";
 export * from "./sun-icon.component";
 export * from "./knits-left-icon.component";
 export * from "./knits-right-icon.component";
+export * from "./telegram-icon.component";
