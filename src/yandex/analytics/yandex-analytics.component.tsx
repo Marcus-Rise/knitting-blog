bc5b8c5c3e41452a48bf77a92cc65b04c1704ac1
Marcus-Rise <ilykonst95@gmail.com>
2023-05-31 19:00:17 +0400

    feat: yandex metric

---
+++ src/yandex/analytics/yandex-analytics.component.tsx
@@ -0,0 +1,40 @@
+import type { FC } from "react";
+import Script from "next/script";
+
+const YandexAnalytics: FC = () => (
+  <>
+    <Script id={"yandex-metric"}>
+      {`(function(m, e, t, r, i, k, a) {
+        m[i] = m[i] || function() {
+          (m[i].a = m[i].a || []).push(arguments);
+        };
+        m[i].l = 1 * new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+          if (document.scripts[j].src === r) {
+            return;
+          }
+        }
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
+      })
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(87044556, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+      });`}
+    </Script>
+    <noscript>
+      <div>
+        <img
+          src="https://mc.yandex.ru/watch/87044556"
+          style={{ position: "absolute", left: "-9999px" }}
+          alt=""
+        />
+      </div>
+    </noscript>
+  </>
+);
+
+export { YandexAnalytics };
