035f88232464bb38d5b9ed0d435ab3dca0817ef4
Marcus-Rise <ilykonst95@gmail.com>
2023-06-14 11:03:55 +0300

    perf: image as server component

--- src/post/components/post-image/post-image.component.tsx
+++ src/post/components/post-image/post-image.component.tsx
@@ -1,13 +1,8 @@
-"use client";
-
 import NextImage from "next/image";
 import type { ComponentProps, FC } from "react";
-import { postImageLoader } from "./post-image-loader.helper";
 
 type Props = ComponentProps<typeof NextImage>;
 
-const PostImage: FC<Props> = (props) => (
-  <NextImage {...props} placeholder={"empty"} loader={postImageLoader} />
-);
+const PostImage: FC<Props> = (props) => <NextImage {...props} placeholder={"empty"} />;
 
 export { PostImage };
